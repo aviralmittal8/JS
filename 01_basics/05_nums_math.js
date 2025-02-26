bade7be3a000a40f1b736d0ae0766e1c3136a642
Aviral <techieaviral11@gmail.com>
2025-02-26 18:35:05 +0530

    Math Operations

--- 01_basics/05_nums_math.js
+++ 01_basics/05_nums_math.js
@@ -1,3 +1,35 @@
 const score = 400
-const balance = new Number(40)
-console.log(balance)
+const balance = new Number(40) // Creating a Number object
+// console.log(score);
+// console.log(balance);
+// console.log(typeof (balance)); // Checking the type of 'balance'
+// console.log(balance.toString().length); // Converting to string and getting length
+// console.log(typeof (balance));
+// console.log(balance.toFixed(2)); // Formatting number to 2 decimal places
+
+// const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(4)); // Rounding with precision of 4 digits
+
+// const hundereds = 10000000
+// console.log(hundereds.toLocaleString('en-IN')); // Formatting number as per Indian locale
+
+
+//*************************Math*****************************//
+
+// console.log(Math.abs(-4)); // Returns absolute value
+// console.log(Math.round(4.5)); // Rounds to the nearest integer
+// console.log(Math.ceil(4.5)); // Rounds up to the nearest integer
+// console.log(Math.floor(4.5)); // Rounds down to the nearest integer
+
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor((Math.random() * (max - min + 1)) + min))
+
+
+
+
